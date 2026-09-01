7667a45fbd38d24cbd7be84e4fe9422a21174fce
Kennedymorgy <claudiokennedymorgy@gmail.com>
2026-09-01 22:04:54 +0200

    Refactor download link retrieval process

--- api/download.js
+++ api/download.js
@@ -9,68 +9,24 @@ module.exports = async (req, res) => {
   }
 
   try {
-    // 1. Acessa a pagina principal do jogo no LiteAPKs
-    const response1 = await axios.get(targetUrl, {
+    const response = await axios.get(targetUrl, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
       }
     });
 
-    const $1 = cheerio.load(response1.data);
-    let downloadPageLink = $1('a[href*="/download/"]').attr('href') || $1('a.btn-download').attr('href');
+    const $ = cheerio.load(response.data);
+    let downloadLink = $('a[href*="/download/"]').attr('href') || $('a.btn-download').attr('href');
 
-    if (!downloadPageLink) {
-      return res.status(444).send('Pagina de download nao encontrada.');
-    }
-
-    if (!downloadPageLink.startsWith('http')) {
-      downloadPageLink = 'https://liteapks.com' + downloadPageLink;
-    }
-
-    // 2. Acessa a pagina intermediaria para pegar o link final do arquivo
-    const response2 = await axios.get(downloadPageLink, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    if (downloadLink) {
+      if (!downloadLink.startsWith('http')) {
+        downloadLink = 'https://liteapks.com' + downloadLink;
       }
-    });
-
-    const $2 = cheerio.load(response2.data);
-    
-    // Procura o link direto de download do APK
-    let directApkLink = $2('a[href*=".apk"]').attr('href') || 
-                        $2('a[download]').attr('href') || 
-                        $2('.download-list a').attr('href');
-
-    if (directApkLink) {
-      if (!directApkLink.startsWith('http')) {
-        directApkLink = 'https://liteapks.com' + directApkLink;
-      }
-      return res.redirect(302, directApkLink);
+      return res.redirect(302, downloadLink);
     } else {
-      return res.redirect(302, downloadPageLink);
+      return res.status(444).send('Botao de download nao encontrado.');
     }
-
   } catch (error) {
-    return res.status(500).send('Erro ao buscar o link direto: ' + error.message);
-  }
-};
-    const $2 = cheerio.load(response2.data);
-    
-    // Procura o link direto de download do APK
-    let directApkLink = $2('a[href*=".apk"]').attr('href') || 
-                        $2('a[download]').attr('href') || 
-                        $2('.download-list a').attr('href');
-
-    if (directApkLink) {
-      if (!directApkLink.startsWith('http')) {
-        directApkLink = 'https://liteapks.com' + directApkLink;
-      }
-      return res.redirect(302, directApkLink);
-    } else {
-      return res.redirect(302, downloadPageLink);
-    }
-
-  } catch (error) {
-    return res.status(500).send('Erro ao buscar o link direto: ' + error.message);
+    return res.status(500).send('Erro na requisição: ' + error.message);
   }
 };
